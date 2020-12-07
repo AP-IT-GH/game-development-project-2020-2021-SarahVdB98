70d4c8d929780998c01a0e0003820e1d0b89a834
Sarah <sarah.vandenbroeck@student.ap.be>
2020-12-07 16:25:19 +0100

    begin levelDesign

--- ProjectGameDev/ProjectGameDev/Game1.cs
+++ ProjectGameDev/ProjectGameDev/Game1.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using ProjectGameDev.Input;
 using ProjectGameDev.Interfaces;
+using ProjectGameDev.LevelDesign;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -15,11 +16,11 @@ namespace ProjectGameDev
         private SpriteBatch _spriteBatch;
         private Texture2D texture;
         private Texture2D brickTexture;
+        private Brick[] brickArr = new Brick[5];
         
         Hero hero;
-        Brick brick;
+        Level level;
         CollisionManager collisionManager;
-
 
         public Game1()
         {
@@ -32,6 +33,8 @@ namespace ProjectGameDev
         {
             // TODO: Add your initialization logic here
             collisionManager = new CollisionManager();
+            level = new Level(Content);
+            level.CreateWorld();
 
             base.Initialize();
         }
@@ -49,8 +52,14 @@ namespace ProjectGameDev
 
         private void InitializeGameObjects()
         {
+            
             hero = new Hero(texture, new KeyBoardReader());
-            brick = new Brick(brickTexture, new Vector2(120, 200));
+            int Xpos = 120;
+            for (int i = 0; i < 5; i++)
+            {
+                brickArr[i] = new Brick(brickTexture, new Vector2(Xpos, 200));
+                Xpos += 50;
+            }
         }
 
 
@@ -59,32 +68,24 @@ namespace ProjectGameDev
             
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
-
-            
-            // TODO: Add your update logic here
             hero.Update(gameTime);
-            brick.Update();
-            if (collisionManager.CheckCollision(hero.CollisionRectangle, brick.CollisionRectangle))
+            level.steen.Update();
+            if (collisionManager.CheckCollision(hero.CollisionRectangle, level.steen.CollisionRectangle))
             {
-                if (hero.positie.X < brick.Positie.X-50)
+                if (hero.positie.X < level.steen.Positie.X)
                 {
-                    hero.positie.X -= 3;
+                    hero.positie.X -= 2;
                 }
-                else if (hero.positie.X > brick.Positie.X)
+                if (hero.positie.Y < level.steen.Positie.Y - 90)
                 {
-                    hero.positie.X += 3;
+                    hero.positie.Y = level.steen.Positie.Y - 90;
                 }
-                else if (hero.positie.Y < brick.Positie.Y-50)
+                else if (hero.positie.Y > level.steen.Positie.Y-90)
                 {
-                    hero.positie.Y = brick.Positie.Y-90;
-                }
-                else if (hero.positie.Y > brick.Positie.Y-50)
-                {
-                    hero.positie.Y += 14;
+                    hero.positie.Y -= 2 ;
                 }
             }
-           
+            //}
             base.Update(gameTime);
         }
 
@@ -93,12 +94,9 @@ namespace ProjectGameDev
             GraphicsDevice.Clear(Color.CornflowerBlue);
             _spriteBatch.Begin();
             hero.Draw(_spriteBatch);
-            brick.Draw(_spriteBatch);
+            level.DrawWorld(_spriteBatch);
             _spriteBatch.End();
             
-
-            // TODO: Add your drawing code here
-
             base.Draw(gameTime);
         }
     }
---
+++ ProjectGameDev/ProjectGameDev/LevelDesign/Level.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectGameDev.LevelDesign
+{
+    class Level
+    {
+        public Texture2D texture;
+        public Brick steen;
+
+        //public byte[,] tileArray = new Byte[,]
+        //{
+        //    {0,0,0,0,0,0 },
+        //    {0,0,0,0,0,0 },
+        //    {0,0,0,0,0,0 },
+        //    {1,0,1,0,1,0 },
+        //    {0,1,0,1,0,1 },
+        //};
+
+        //public Brick[,] blokArray = new Brick[5, 6];
+
+        private ContentManager content;
+
+        public Level(ContentManager content)
+        {
+            this.content = content;
+
+            InitializeContent();
+        }
+
+        private void InitializeContent()
+        {
+            texture = content.Load<Texture2D>("wall");
+        }
+
+
+        public void CreateWorld()
+        {
+            steen = new Brick(texture, new Vector2(128, 250));
+
+            //for (int x = 0; x < 5; x++)
+            //{
+            //    for (int y = 0; y < 6; y++)
+            //    {
+            //        if (tileArray[x, y] == 1)
+            //        {
+            //           blokArray[x, y] = new Brick(texture, new Vector2(y * 128, x * 64));
+            //        }
+            //    }
+            //}
+        }
+
+        public void DrawWorld(SpriteBatch spritebatch)
+        {
+            steen.Draw(spritebatch);
+            //for (int x = 0; x < 5; x++)
+            //{
+            //    for (int y = 0; y < 6; y++)
+            //    {
+            //        if (blokArray[x, y] != null)
+            //        {
+            //            blokArray[x, y].Draw(spritebatch);
+            //        }
+            //    }
+            //}
+
+        }
+    
+}
+}
