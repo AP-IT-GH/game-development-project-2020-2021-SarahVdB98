c815aebbc6f15d20638944b95f27000b4dea6bac
Sarah <sarah.vandenbroeck@student.ap.be>
2020-12-07 15:33:24 +0100

    collision met array zonder level design

--- ProjectGameDev/ProjectGameDev/Game1.cs
+++ ProjectGameDev/ProjectGameDev/Game1.cs
@@ -15,9 +15,9 @@ namespace ProjectGameDev
         private SpriteBatch _spriteBatch;
         private Texture2D texture;
         private Texture2D brickTexture;
+        private Brick[] brickArr = new Brick[5];
         
         Hero hero;
-        Brick brick;
         CollisionManager collisionManager;
 
 
@@ -49,8 +49,14 @@ namespace ProjectGameDev
 
         private void InitializeGameObjects()
         {
+            
             hero = new Hero(texture, new KeyBoardReader());
-            brick = new Brick(brickTexture, new Vector2(120, 200));
+            int Xpos = 120;
+            for (int i = 0; i < 5; i++)
+            {
+                brickArr[i] = new Brick(brickTexture, new Vector2(Xpos, 200));
+                Xpos += 50;
+            }
         }
 
 
@@ -59,32 +65,22 @@ namespace ProjectGameDev
             
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
-
-            
-            // TODO: Add your update logic here
             hero.Update(gameTime);
-            brick.Update();
-            if (collisionManager.CheckCollision(hero.CollisionRectangle, brick.CollisionRectangle))
+            for (int i = 0; i < brickArr.Length; i++)
             {
-                if (hero.positie.X < brick.Positie.X-50)
+                brickArr[i].Update();
+                if (collisionManager.CheckCollision(hero.CollisionRectangle, brickArr[i].CollisionRectangle))
                 {
-                    hero.positie.X -= 3;
-                }
-                else if (hero.positie.X > brick.Positie.X)
-                {
-                    hero.positie.X += 3;
-                }
-                else if (hero.positie.Y < brick.Positie.Y-50)
-                {
-                    hero.positie.Y = brick.Positie.Y-90;
-                }
-                else if (hero.positie.Y > brick.Positie.Y-50)
-                {
-                    hero.positie.Y += 14;
+                    if (hero.positie.Y < brickArr[i].Positie.Y - 50)
+                    {
+                        hero.positie.Y = brickArr[i].Positie.Y - 90;
+                    }
+                    else if (hero.positie.Y > brickArr[i].Positie.Y - 50)
+                    {
+                        hero.positie.Y += 14;
+                    }
                 }
             }
-           
             base.Update(gameTime);
         }
 
@@ -93,12 +89,12 @@ namespace ProjectGameDev
             GraphicsDevice.Clear(Color.CornflowerBlue);
             _spriteBatch.Begin();
             hero.Draw(_spriteBatch);
-            brick.Draw(_spriteBatch);
+            for (int i = 0; i < brickArr.Length; i++)
+            {
+                brickArr[i].Draw(_spriteBatch);
+            }
             _spriteBatch.End();
             
-
-            // TODO: Add your drawing code here
-
             base.Draw(gameTime);
         }
     }
---
+++ ProjectGameDev/ProjectGameDev/LevelDesign/Level.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectGameDev.LevelDesign
+{
+    class Level
+    {
+        public Texture2D texture;
+        public Vector2 pos = new Vector2(250, 340);
+
+        public byte[,] tileArray = new Byte[,]
+        {
+            {0,0,0,0,0,0 },
+            {0,0,0,0,0,0 },
+            {1,0,1,0,1,0 },
+            {0,1,0,1,0,1 },
+
+        };
+        private Brick[,] brickArray = new Brick[4,6];
+
+        private ContentManager content;
+
+        public Level(ContentManager content)
+        {
+            this.content = content;
+            InitializeContent();
+
+        }
+
+        private void InitializeContent()
+        {
+            texture = content.Load<Texture2D>("wall");
+        }
+
+        public void CreateWorld()
+        {
+            for (int x = 0; x < 4; x++)
+            {
+                for (int y = 0; y < 6; y++)
+                {
+                    if (tileArray[x,y] == 1)
+                    {
+                        brickArray[x, y] = new Brick(texture, new Vector2(y *128, x * 90));
+                    }
+                }
+            }
+
+        }
+
+        public void DrawWorld(SpriteBatch spriteBatch)
+        {
+            for (int x = 0; x < 4; x++)
+            {
+                for (int y = 0; y < 6; y++)
+                {
+                    if (brickArray[x,y] != null)
+                    {
+                        brickArray[x, y].Draw(spriteBatch);
+                    }
+                }
+            }
+        }
+    }
+}
