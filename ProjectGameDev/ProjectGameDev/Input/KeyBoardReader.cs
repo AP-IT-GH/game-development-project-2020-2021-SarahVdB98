f1ab07af8670753d8b901725728b69b1520eba51
Sarah <sarah.vandenbroeck@student.ap.be>
2020-12-07 15:33:24 +0100

    collision met list zonder level design

--- ProjectGameDev/ProjectGameDev/Input/KeyBoardReader.cs
+++ ProjectGameDev/ProjectGameDev/Input/KeyBoardReader.cs
@@ -12,21 +12,23 @@ namespace ProjectGameDev.Input
         public Vector2 ReadInput()
         {
             var direction = Vector2.Zero;
-            KeyboardState state = Keyboard.GetState();
-            if (state.IsKeyDown(Keys.Left))
+            KeyboardState newstate = Keyboard.GetState();
+            KeyboardState oldstate = Keyboard.GetState();
+            if (newstate.IsKeyDown(Keys.Left))
             {
                 direction = new Vector2(-1, 0);
             }
-            if (state.IsKeyDown(Keys.Right))
+            if (newstate.IsKeyDown(Keys.Right))
             {
                 direction = new Vector2(1, 0);
             }
             
-            if (state.IsKeyDown(Keys.Space))
+            if (oldstate.IsKeyDown(Keys.Space))
             {
                 direction = new Vector2(0, -5);
             }
             direction *= 3;
+            oldstate = newstate;
 
             return direction;
         }
